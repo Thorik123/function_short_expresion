4dc202316ddc4232e39b0247a3be67c0d140a4b5
Thorik123 <tronikthorik07@gmail.com>
2022-01-09 19:09:58 +0700

    merubah

--- test/function_short_expresion_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:function_short_expresion/function_short_expresion.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
